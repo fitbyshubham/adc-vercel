e3f3b839cdfe0856493c78d35a860149d19bdbae
imSmaity <sumanmaity305@gmail.com>
2024-02-07 17:20:56 +0530

    header design

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const NavLink = ({ title, path }) => {
+  return (
+    <a className="font-semibold text-[20px]" href={path}>
+      {title}
+    </a>
+  )
+}
+const Navbar = () => {
+  const linkList = [
+    { title: 'ADC', path: '#' },
+    { title: 'START', path: '#' },
+    { title: 'MENU', path: '#' },
+  ]
+
+  return (
+    <div className="flex justify-between p-4 fixed w-full">
+      {linkList.map((item) => (
+        <NavLink key={item.title} title={item.title} path={item.path} />
+      ))}
+    </div>
+  )
+}
+
+export default Navbar
