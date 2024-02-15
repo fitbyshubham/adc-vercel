3f82921c2d00c459d8e8de8857b94d71a02be6c6
imSmaity <sumanmaity305@gmail.com>
2024-02-15 09:40:02 +0530

    Api instance added

---
+++ app/api/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import config from '../apiConfig'
+
+const axiosInstance = function (token) {
+  return axios.create({
+    baseURL: config.API_URL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/json, text/plain, */*',
+    },
+  })
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  getHomePage() {
+    return new Promise((resolve, reject) => {
+      axiosInstance(config.token)
+        .get(config.HOMEPAGE.BASE.concat('?populate=*'))
+        .then((res) => resolve(res.data))
+        .catch((error) => reject(error))
+    })
+  },
+}
---
+++ app/apiConfig/index.js
@@ -0,0 +1,9 @@
+const config = {
+  API_URL: process.env.API_URL,
+  token: process.env.API_TOKEN,
+  HOMEPAGE: {
+    BASE: 'homepage',
+  },
+}
+
+export default config
