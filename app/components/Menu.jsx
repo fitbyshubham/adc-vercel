c061823ba940f47ae047cb5c12a245fbc732ab8e
Krish Tak <takrishtak2002@gmail.com>
2024-03-08 12:32:45 +0530

    Fixed overall responsiveness for navbar and other pages

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -15,49 +15,57 @@ const Menu = ({ open, handleClose }) => {
   const twClass = open ? "bg-white fixed w-full z-50" : "hidden"
   return (
     <div className={twClass}>
-      <div className="flex justify-between p-4 pb-10">
-        <NavLink title={"ADC"} path={""} />
-        <div className="flex items-center gap-20">
-          <div>
-            <div className="flex gap-4">
-              {menuLinkList.map((item) => (
-                <NavLink
-                  key={item.title}
-                  title={item.title.toUpperCase()}
-                  fontSize={12}
-                  path={item.path}
-                />
-              ))}
-            </div>
-            <div className="text-center pt-10">
-              <Text twClassName={"text-[54px]"} onClick={handleClose}>
-                <Link href={"/insights"}>INSIGHTS</Link>
-              </Text>
-              <Text twClassName={"text-[54px]"} onClick={handleClose}>
-                <Link href={"/learning"}>LEARNING</Link>
-              </Text>
-              <Text twClassName={"text-[54px]"} onClick={handleClose}>
-                <Link href={"/creative-days"}>Creative Days</Link>
-              </Text>
-              <Text twClassName={"text-[54px]"} onClick={handleClose}>
-                <Link href={"/awards"}>ADC AWARDs</Link>
-              </Text>
-              <Text twClassName={"text-[54px]"} onClick={handleClose}>
-                <Link href={"/people"}>PEOPLE</Link>
-              </Text>
-            </div>
+      <div className="flex flex-col gap-4 p-4 pb-10">
+        <div className="flex justify-between gap-x-8">
+          <NavLink title={"ADC"} path={""} />
+
+          <div className="flex flex-wrap justify-center gap-x-8 gap-y-4">
+            {menuLinkList.map((item) => (
+              <NavLink
+                key={item.title}
+                title={item.title.toUpperCase()}
+                fontSize={12}
+                path={item.path}
+              />
+            ))}
           </div>
-          {/* <div className="flex gap-10">
-            <div>FR</div>
-            <div>LOGIN</div>
-          </div> */}
-        </div>
-        <div className="flex">
           <NavLink
             title={open ? "CLOSE" : "MENU"}
             path={""}
             handleClick={handleClose}
           />
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <Text
+            twClassName={"md:text-[54px] text-[40px]"}
+            onClick={handleClose}
+          >
+            <Link href={"/insights"}>INSIGHTS</Link>
+          </Text>
+          <Text
+            twClassName={"md:text-[54px] text-[40px]"}
+            onClick={handleClose}
+          >
+            <Link href={"/learning"}>LEARNING</Link>
+          </Text>
+          <Text
+            twClassName={"md:text-[54px] text-[40px]"}
+            onClick={handleClose}
+          >
+            <Link href={"/creative-days"}>Creative Days</Link>
+          </Text>
+          <Text
+            twClassName={"md:text-[54px] text-[40px]"}
+            onClick={handleClose}
+          >
+            <Link href={"/awards"}>ADC AWARDs</Link>
+          </Text>
+          <Text
+            twClassName={"md:text-[54px] text-[40px]"}
+            onClick={handleClose}
+          >
+            <Link href={"/people"}>PEOPLE</Link>
+          </Text>
         </div>
       </div>
     </div>
